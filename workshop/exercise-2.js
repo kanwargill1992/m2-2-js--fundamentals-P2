555490a54b10fc9aa91dc80cefc8d9d762c3b48b
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-16 23:52:07 -0400

    Q2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,7 +3,15 @@
 // the greater one.
 
 // Define the function
-function max(num1, num2) {}
+function max(num1, num2) {
+    const biggerValue;
+    if(num1>num2){
+        biggerValue=num1;
+    } else {
+        biggerValue=num2;
+    }
+    console.log(biggerValue + ''+ "is the bigger number out of these 2 numbers");
+}
 
 // Call the function
 max(12, 43);
