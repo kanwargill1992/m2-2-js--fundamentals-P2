b2eb24b6ac6623d723aa4913e0f72674f3381db3
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-16 23:53:17 -0400

    Q12

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -9,6 +9,29 @@
 // <100 is A
 
 // Write function here
+function displayContent(){
+
+    for (const grade = 0; grade < numericalGrade.length; grade++) {
+if (gradeData[grade] >= 100) {
+
+    document.write( "" + nameData[grade] + "A");
+}
+else if (gradeData[grade]>= 90) {
+
+    document.write("" + nameData[i] + "B");
+}
+else if (gradeData[grade]>= 80) {
+    document.write("" + nameData[i] + "C");
+}
+else if (gradeData[grade]>= 70) {
+    document.write("" + nameData[i] + "D");
+}
+else  if (gradeData[grade]>=60){
+    document.write("" + nameData[i] + "F");
+}
+    }
+}
+
 
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
