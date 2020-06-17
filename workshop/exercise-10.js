a7915535689bfe962e69a3668b3cacc5dd3dd540
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-17 15:56:16 -0400

    V2 Q10

--- workshop/exercise-10.js
+++ workshop/exercise-10.js
@@ -7,14 +7,14 @@
 // Returns true if every element of lst is of length at least 5.
 // Otherwise returns false.
 function allLong(lst) {
-  return lst.all(function (name){
+  return lst.every(function (name) {
     return name.length > 4;
   });
   // lst is an array of strings
 }
 // -------------------------------------------------------------------------
-console.log('Q8 - case 1', allLong(['Scott', 'Bob', 'Ric', 'Jim']));
+console.log("Q8 - case 1", allLong(["Scott", "Bob", "Ric", "Jim"]));
 console.log(
-  'Q8 - case 2',
-  allLong(['Cyborg', 'Robin', 'Batman', 'Superman', 'Aquaman', 'Flash'])
+  "Q8 - case 2",
+  allLong(["Cyborg", "Robin", "Batman", "Superman", "Aquaman", "Flash"])
 );
